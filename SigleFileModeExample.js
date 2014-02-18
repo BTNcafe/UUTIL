643c6210f3e8d827ce3458880d37dc0339dbde18
BTNcafe <admin@btncafe.com>
2014-02-18 13:28:47 +0900

    no message

---
+++ SigleFileModeExample.js
@@ -0,0 +1,50 @@
+// run: nodemon WebPageExample.js
+
+require('./UPPERCASE_ONE/BOOT.js');
+
+BOOT({
+	CONFIG : {
+		isDevMode : true
+	},
+	SERVER_CONFIG : {
+		isNotUseDB : true
+	},
+	BROWSER_CONFIG : {
+		MAIN : function(BOX) {
+
+			BOX.MATCH_VIEW({
+				uris : ['', 'View'],
+				target : CLASS({
+
+					preset : function() {'use strict';
+						return VIEW;
+					},
+
+					init : function(cls, inner, self) {'use strict';
+
+						var
+						// div
+						div,
+
+						// close.
+						close;
+
+						div = DIV({
+							style : {
+								fontSize : 50
+							},
+							children : [SPAN({
+								children : ['Hello UPPERCASE!']
+							})]
+						}).appendTo(BODY);
+
+						//OVERRIDE: self.close
+						self.close = close = function(params) {
+							div.remove();
+						};
+					}
+				})
+			});
+		}
+	}
+});
---
+++ UPPERCASE_ONE/BOOT.js
@@ -0,0 +1,13 @@
+require('../UPPERCASE/BOOT.js');
+
+var
+// origin BOOT
+_BOOT = BOOT;
+
+BOOT = function(params) {
+	if (params.CONFIG === undefined) {
+		params.CONFIG = {};
+	}
+	params.CONFIG.defaultBoxName = 'UPPERCASE_ONE';
+	_BOOT(params);
+};
