ccd316c10de500ea64f2341738bcd58762a8468c
ias <iasandcb@gmail.com>
2014-03-08 02:51:05 +0900

    더 간단한 형태의 코드 작성을 가능하게 했다

--- UPPERCASE_ONE/BOOT.js
+++ UPPERCASE_ONE/BOOT.js
@@ -11,3 +11,35 @@ BOOT = function(params) {
 	params.CONFIG.defaultBoxName = 'UPPERCASE_ONE';
 	_BOOT(params);
 };
+
+P = function(callback) {
+	var params = {
+	  CONFIG: {
+	    isDevMode: true
+	  },
+	  SERVER_CONFIG: {
+	    isNotUsingDB: true
+	  },
+	  BROWSER_CONFIG: {
+	    P_MAIN: callback
+	  }
+	};
+
+	BOOT(params);
+};
+
+BODY = function(callback) {
+	var params = {
+	CONFIG: {
+		isDevMode: true
+	},
+	SERVER_CONFIG: {
+		isNotUsingDB: true
+	},
+	BROWSER_CONFIG: {
+		BODY_MAIN: callback
+	}
+	};
+
+	BOOT(params);
+};
